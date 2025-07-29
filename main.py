9aaa6b223363116aaaaeb48bbdd0ff901b47ce7f
KYI-m2 <webapplication.m2@gmail.com>
2025-07-29 16:00:33 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import streamlit_option_menu
+import pandas as pd
+with st.sidebar :
+  tab = option_menu (
+    "Sci-Day",
+    ['Home','About Sci-Day','Activities'],
+    icon = ['home','book','game']
+  )
+if tab == 'Home' :
+  st.header('Welcome to Sci-Day")
+  st.text('ง๗ง')
+if tab == 'About Sci-Day' :
+  st.text('deee')
+if tab == 'Activities':
